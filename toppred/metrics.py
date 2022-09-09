c4a8867f47f7c1b68750c941ccbda65462833158
Thijs van Ede <t.s.vanede@utwente.nl>
2022-09-09 13:21:23 +0200

    Added top_classification_report

---
+++ toppred/metrics.py
@@ -0,0 +1,132 @@
+# Imports
+from sklearn.metrics import classification_report
+from toppred.predictions import top_predictions
+from toppred.types import array_like_1d, array_like_2d
+from toppred.utils import reports2string
+from typing import List, Literal, Optional, Union
+
+def top_classification_report(
+        y_true       : array_like_1d,
+        y_pred       : array_like_2d,
+        labels       : Optional[array_like_1d] = None,
+        target_names : Optional[List[str]] = None,
+        sample_weight: Optional[array_like_1d] = None,
+        digits       : int = 2,
+        output_dict  : bool = False,
+        zero_division: Literal["warn", 0, 1] = "warn",
+    ) -> Union[str, dict]:
+    """Create a classification report for a y_pred containing multiple top
+        predictions. This function follows the same API as
+        ``sklearn.metrics.classification_report`` with the exception that:
+        1. ``y_pred`` should be given as a 2D array instead of a 1D array.
+        2. If ``output_dict`` is ``True``, the output dictionary consists of a
+        dictionary where ``key`` is 0-indexed top prediction and ``value`` is
+        the ``sklearn.metrics.classification_report`` output dictionary for that
+        top prediction.
+        
+        Parameters
+        ----------
+        y_true : array_like_1d of shape=(n_samples,)
+            Ground truth (correct) target values.
+
+        y_pred : array_like_2d of shape=(n_samples, n_predictions)
+            Estimated targets as returned by a classifier. Each column
+            y_pred[:, i] indicates the i-th most likely prediction (0-indexed)
+            for the given sample.
+
+        labels : Optional[array_like_1d], default = None
+            Optional list of label indices to include in the report.
+
+        target_names : Optional[List[str]] = None
+            Optional display names matching the labels (same order).
+
+        sample_weight : Optional[array_like_1d], default = None
+            Sample weights.
+
+        digits : int, default = 2
+            Number of digits for formatting output floating point values. When
+            ``output_dict`` is ``True``, this will be ignored and the returned
+            values will not be rounded.
+
+        output_dict : bool, default = False
+            If True, return output as dict.
+
+        zero_division : Union[Literal["warn"], 0, 1], default = "warn"
+            Sets the value to return when there is a zero division. If set to
+            “warn”, this acts as 0, but warnings are also raised.
+        
+        Returns
+        -------
+        report : Union[str, dict]
+            Text summary of the precision, recall, F1 score for each class.
+            Dictionary returned if output_dict is True.
+
+            The reported averages include macro average (averaging the
+            unweighted mean per label), weighted average (averaging the support-
+            weighted mean per label), and sample average (only for multilabel
+            classification). Micro average (averaging the total true positives,
+            false negatives and false positives) is only shown for multi-label
+            or multi-class with a subset of classes, because it corresponds to
+            accuracy otherwise and would be the same for all metrics. See also
+            precision_recall_fscore_support for more details on averages.
+
+            Note that in binary classification, recall of the positive class is
+            also known as “sensitivity”; recall of the negative class is
+            “specificity”.
+        """
+    # Create dictionary_based classification reports for each top prediction
+    reports = [
+        classification_report(
+            y_true        = y_true,
+            y_pred        = y_pred_,
+            labels        = labels,
+            target_names  = target_names,
+            sample_weight = sample_weight,
+            digits        = digits,
+            output_dict   = True,
+            zero_division = zero_division,
+        )
+        for _, y_pred_ in top_predictions(y_true=y_true, y_pred=y_pred)
+    ]
+
+    # Return report as dictionary, if necessary
+    if output_dict:
+        return {i: report for i, report in enumerate(reports)}
+    # Otherwise return reports as string
+    else:
+        return reports2string(reports, digits)
+
+
+
+if __name__ == "__main__":
+    import numpy as np
+    y_true = np.asarray([1, 2, 3, 4, 5, 4, 3, 2, 1])
+    y_pred = np.asarray([
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3],
+    ])
+
+    print(classification_report(
+        y_true = y_true,
+        y_pred = y_pred[:, 0],
+        labels = [0, 1, 2, 3, 4, 5],
+        target_names = ['None', 'one', 'two', 'three', 'four', 'five'],
+        digits = 4,
+        zero_division = 0,
+    ))
+
+    print(top_classification_report(
+        y_true = y_true,
+        y_pred = y_pred,
+        labels = [0, 1, 2, 3, 4, 5],
+        target_names = ['None', 'one', 'two', 'three', 'four', 'five'],
+        digits = 4,
+        zero_division = 0,
+    ))
---
+++ toppred/utils.py
@@ -0,0 +1,76 @@
+# Imports
+import numpy as np
+import pandas as pd
+from typing import List
+
+def reports2string(reports: List[dict], digits: int = 2) -> str:
+    """Convert a list of classification report dictionaries to a string.
+    
+        Parameters
+        ----------
+        reports : List[dict]
+            List of classification reports to convert to string.
+            Should be produced with ``sklearn.metrics.classification_report``
+            where ``output_dict = True``.
+
+        digits : int, default = 2
+            Number of digits of precision to use.
+            
+        Returns
+        -------
+        report : str
+            String representation of list of classification reports.
+        """
+    # Initialise dataframes
+    dataframes = list()
+
+    # Loop over all reports
+    for top, report in enumerate(reports):
+        # Initialise labels and metrics
+        labels  = list()
+        metrics = dict()
+
+        # Collect all labels and metrics in report
+        for label, performance in report.items():
+            # Add space before micro avg
+            if label == 'micro avg':
+                labels.append('')
+                for key in metrics:
+                    metrics[key].append('')
+
+            # Add label
+            labels.append(label)
+            # Add all metrics
+            for metric, value in performance.items():
+
+                # Set precision
+                if metric is not 'support':
+                    # Set MultiIndex
+                    metric = (f"Top {top+1}", metric, '')
+
+                    # Set precision value
+                    value = f"{value:.{digits}f}"
+
+                    # Add metric
+                    if metric not in metrics:
+                        metrics[metric] = list()
+                    metrics[metric].append(value)
+
+            if top == len(reports) - 1:
+                if ('', 'support', '') not in metrics:
+                    metrics[('', 'support', '')] = list()
+                metrics[('', 'support', '')].append(performance['support'])
+
+        # Transform to dataframe
+        dataframes.append(pd.DataFrame(metrics, index=labels))
+
+    # Merge dataframes
+    dataframe = pd.concat(dataframes, axis=1)
+
+    # Style dataframe
+    dataframe.style.set_properties(**{
+        'text-align': 'right'
+    })
+
+    # Return dataframe as string
+    return str(dataframe)
