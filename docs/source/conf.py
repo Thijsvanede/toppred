beb362bc86ee7b72bf04b71486c777c2ee4c116e
Thijs van Ede <t.s.vanede@utwente.nl>
2022-09-19 11:13:44 +0200

    Fixed import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import sys
-sys.path.insert(0, os.path.abspath('../../toppred/'))
-
+import pathlib
+sys.path.insert(0, pathlib.Path(__file__).parent.parent.parent / 'toppred')
 
 # -- Project information -----------------------------------------------------
 
